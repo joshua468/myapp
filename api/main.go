a5cd867638c07fd116f25f765e511163c591549c
joshua468 <adegbesanjoshua9@gmail.com>
2024-07-05 00:48:07 +0100

    Set up Vercel configuration

---
+++ api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/joshua468/myapp/api"
+)
+
+func main() {
+	http.HandleFunc("/api/hello", api.Handler)
+
+	port := ":8081" // Define the port
+	log.Println("Server is running on port", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("could not start server: %s\n", err)
+	}
+}
